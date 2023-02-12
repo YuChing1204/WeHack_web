98e87f96f5c13801060070630eee45aee715ae8b
c20804 <69203454+c20804@users.noreply.github.com>
2023-02-12 02:18:17 -0600

    community_modified

--- client/src/components/community_component.js
+++ client/src/components/community_component.js
@@ -1,43 +1,71 @@
 import EventComponent from "./event_component";
 
 const CommunityComponent = () => {
-//     const [data,setData]=useState([]);
+    // let { currentUser, setCurrentUser } = props;
+// !currentUser.user.community
+let test = true;
 
-//     //get data from DB
-//     const getData = () => {
-//         EventService.getAll()
-//         .then((data) => {
-//           console.log(data);
-//           setData(data.data);
-//           setFiltered(data.data);
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-        
-//     }
 
-//     useEffect(()=>{
-//         getData()
-//     },[])
+
+    //     const [data,setData]=useState([]);
+
+    //     //get data from DB
+    //     const getData = () => {
+    //         EventService.getAll()
+    //         .then((data) => {
+    //           console.log(data);
+    //           setData(data.data);
+    //           setFiltered(data.data);
+    //         })
+    //         .catch((err) => {
+    //           console.log(err);
+    //         });
+
+    //     }
+
+    //     useEffect(()=>{
+    //         getData()
+    //     },[])
 
     return (
         <div className="row" id="listing">
-            <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
-                <EventComponent />
-            </div>
-            <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
-                <EventComponent />
-            </div>
-            <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
-                <EventComponent />
-            </div>
-            <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
-                <EventComponent />
-            </div>
-            <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
-                <EventComponent />
-            </div>
+            {
+                !test && (
+                    <div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+                        <div>You must belong to a community!</div>
+
+                    </div>
+                )
+            }
+
+            {test && (
+                <div>
+                    <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
+                        <EventComponent />
+                    </div>
+                    <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
+                        <EventComponent />
+                    </div>
+                    <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
+                        <EventComponent />
+                    </div>
+                    <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
+                        <EventComponent />
+                    </div>
+                    <div className="row col-sm-2 col-md-4 col-lg-4 g-4">
+                        <EventComponent />
+                    </div>
+                </div>
+
+            )}
+
         </div>
     )
 }
